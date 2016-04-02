560377184db7e0b63e036e94d285a34bb794f29e
Cristina Gallego <cristina@ubuntu>
2016-04-02 18:32:40 +0200

    terminado

--- myproject/urls.py
+++ myproject/urls.py
@@ -5,6 +5,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'(.+)/(.+)', 'cms.views.savepage'),
+    url(r'^$', 'cms.views.showpages'),
+    url(r'(.+)', 'cms.views.newpages'),
+    url(r'(.*)/(.*)', 'cms.views.Error404'),
+
 )
